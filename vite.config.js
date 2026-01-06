fbbe8c7eedbd7876b7fd19804ae5c4f2125ab7be
Anjali Thakur <thakur.anjali0405@gmail.com>
2026-01-07 01:43:06 +0530

    build setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/react-portfolio/",
+  base: "/react-portfolio/", // <-- required for GitHub Pages
   plugins: [react()],
 });
